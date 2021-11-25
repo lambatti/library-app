22aa9f1faec06b5d31ae4fbd113e312e31b449e8
lambatti <adamgrzegorzgala@gmail.com>
2021-11-25 18:58:48 +0100

    Initial commit

---
+++ src/main/java/com/software/architecture/libraryapp/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.software.architecture.libraryapp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+@Slf4j
+public class TestController {
+    @GetMapping("/test")
+    public void test() {
+        log.info("test");
+    }
+}
