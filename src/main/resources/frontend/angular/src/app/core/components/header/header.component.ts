f27ab50c646e4f94b5378ae9f7e802d3b3727507
Kamil <k-gryciuk@wp.pl>
2021-12-03 18:42:09 +0100

    Configured prettier

--- src/main/resources/frontend/angular/src/app/core/components/header/header.component.ts
+++ src/main/resources/frontend/angular/src/app/core/components/header/header.component.ts
@@ -1,10 +1,7 @@
-import {Component} from "@angular/core";
-
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'appHeader',
-  templateUrl: 'header.component.html'
+    selector: 'appHeader',
+    templateUrl: 'header.component.html'
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
