f14355a35078991dea514371e570b86b69657d28
lambatti <adamgrzegorzgala@gmail.com>
2021-12-28 08:50:21 +0100

    Modified migrations (added password and roles),fixed query in BookBorrow

--- src/main/java/com/software/architecture/libraryapp/adapter/SqlBookBorrowRepository.java
+++ src/main/java/com/software/architecture/libraryapp/adapter/SqlBookBorrowRepository.java
@@ -13,4 +13,7 @@ public interface SqlBookBorrowRepository extends BookBorrowRepository, JpaReposi
     @Query(nativeQuery = true, value="SELECT return_date FROM book_borrow WHERE book_borrow.book_id = ?1")
     LocalDate getReturnDate(String bookId);
 
+    @Override
+    @Query(nativeQuery = true, value="UPDATE book_borrow SET return_date = ?2 WHERE book_borrow.book_id = ?1")
+    BookBorrow updateById(String bookId, LocalDate returnDate);
 }
--- src/main/resources/db/migration/V3__init_users_table.sql
+++ src/main/resources/db/migration/V3__init_users_table.sql
@@ -5,6 +5,8 @@ CREATE TABLE users
     first_name                      VARCHAR(100) NOT NULL,
     last_name                       VARCHAR(100) NOT NULL,
     email                           VARCHAR(100) NOT NULL,
+    password                        VARCHAR(100) NOT NULL,
+    roles                           VARCHAR(100)[] NOT NULL,
     registration_question           VARCHAR(20)  NOT NULL,
     registration_question_answer    VARCHAR(100) NOT NULL,
     gender                          CHAR(1)      NOT NULL,
