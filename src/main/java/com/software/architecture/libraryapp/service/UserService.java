b85d465754e1424cf858e0a0d242eebf7f41617d
lambatti <adamgrzegorzgala@gmail.com>
2021-12-29 19:24:13 +0100

    Added JWT request filter, added @Service annotation to the UserService

--- src/main/java/com/software/architecture/libraryapp/service/UserService.java
+++ src/main/java/com/software/architecture/libraryapp/service/UserService.java
@@ -7,10 +7,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserService implements UserDetailsService {
 
     private final SqlUserRepository userRepository;
