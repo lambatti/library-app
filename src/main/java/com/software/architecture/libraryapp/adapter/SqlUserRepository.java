f668cd03f5367cadcb5ec13d2c592f1753cae305
lambatti <adamgrzegorzgala@gmail.com>
2022-01-06 20:41:25 +0100

    Updated UserRepository

--- src/main/java/com/software/architecture/libraryapp/adapter/SqlUserRepository.java
+++ src/main/java/com/software/architecture/libraryapp/adapter/SqlUserRepository.java
@@ -4,10 +4,12 @@ import com.software.architecture.libraryapp.model.User;
 import com.software.architecture.libraryapp.repository.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SqlUserRepository extends UserRepository, JpaRepository<User,Integer> {
 
     @Override
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     @Override
     User save(User user);
--- src/main/java/com/software/architecture/libraryapp/repository/UserRepository.java
+++ src/main/java/com/software/architecture/libraryapp/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.software.architecture.libraryapp.repository;
 
 import com.software.architecture.libraryapp.model.User;
 
-public interface UserRepository {
-    User findByEmail(String email);
+import java.util.Optional;
 
+public interface UserRepository {
+    Optional<User> findByEmail(String email);
     User save(User user);
 }
