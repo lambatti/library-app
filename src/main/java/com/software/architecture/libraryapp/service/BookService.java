94629b253d01337c585c57311c44fdf35edec688
gstfnk <gabrysia.stefaniak@gmail.com>
2022-01-07 16:05:54 +0100

    add BookService

--- src/main/java/com/software/architecture/libraryapp/service/BookService.java
+++ src/main/java/com/software/architecture/libraryapp/service/BookService.java
@@ -4,6 +4,7 @@ import com.software.architecture.libraryapp.model.Book;
 import com.software.architecture.libraryapp.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,4 +18,18 @@ public class BookService {
     public Optional<Book> findById(Integer id) {
         return repository.findById(id);
     }
+
+    public List<Book> readAll() {
+        return repository.findAll();
+    }
+
+    public List<Book> readAvailableBooks(boolean available) {
+        return repository.findByIsAvailable(available);
+    }
+
+    public List<Book> readFilteredBooks(
+            String inputTitle, String inputAuthor, String inputGenre) {
+        return repository.findAllByFilters(inputTitle, inputAuthor, inputGenre);
+    }
+
 }
