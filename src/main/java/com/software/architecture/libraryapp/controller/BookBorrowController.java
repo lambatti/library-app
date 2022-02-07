aff2d9063b5e42f789f07e71a59a570f786439fa
lambatti <adamgrzegorzgala@gmail.com>
2022-02-07 23:41:53 +0100

    Updated BookBorrowController

--- src/main/java/com/software/architecture/libraryapp/controller/BookBorrowController.java
+++ src/main/java/com/software/architecture/libraryapp/controller/BookBorrowController.java
@@ -29,66 +29,40 @@ public class BookBorrowController {
 
         Optional<User> user = userService.getUserByToken(token);
 
-        if(user.isEmpty()) {
+        if(user.isEmpty() || bookService.findById(id).isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
 
-        if(bookService.findById(id).isEmpty()) {
-            return ResponseEntity.badRequest().build();
-        }
+        bookBorrowService.borrowBook(user.get(), id);
 
-        else {
-            BookBorrow bookBorrow = bookBorrowService.borrowBook(user.get(), id);
-
-            // TODO: 08.01.2022 - return bookBorrow (probably with DTO) or the book
-            //return ResponseEntity.ok(bookBorrow);
-
-            return ResponseEntity.ok().build();
-        }
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/user/returnBook/{id}")
-    ResponseEntity<BookBorrow> returnBook(@RequestHeader(name="Authorization") String token, @PathVariable Integer id) {
+    ResponseEntity<?> returnBook(@RequestHeader(name="Authorization") String token, @PathVariable Integer id) {
 
         Optional<User> user = userService.getUserByToken(token);
 
-        if(user.isEmpty()) {
+        if(user.isEmpty() || bookService.findById(id).isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
 
-        if(bookService.findById(id).isEmpty()) {
-            return ResponseEntity.badRequest().build();
-        }
+        bookBorrowService.returnBook(user.get(), id);
 
-        else {
-            BookBorrow bookBorrow = bookBorrowService.returnBook(user.get(), id);
-
-            // TODO: 08.01.2022 - return bookBorrow (probably with DTO) or the book
-            //return ResponseEntity.ok(bookBorrow);
-
-            return ResponseEntity.ok().build();
-        }
+        return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/user/prolongate/{id}")
-    ResponseEntity<BookBorrow> prolongate(@RequestHeader(name="Authorization") String token, @PathVariable Integer id) {
+    ResponseEntity<?> prolongate(@RequestHeader(name="Authorization") String token, @PathVariable Integer id) {
 
         Optional<User> user = userService.getUserByToken(token);
 
-        if (user.isEmpty()) {
+        if (user.isEmpty() || bookService.findById(id).isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
 
-        if (bookService.findById(id).isEmpty()) {
-            return ResponseEntity.badRequest().build();
-        } else {
-            BookBorrow bookBorrow = bookBorrowService.prolongate(user.get(), id);
+        bookBorrowService.prolongate(user.get(), id);
 
-            // TODO: 08.01.2022 - return bookBorrow (probably with DTO)
-            //return ResponseEntity.ok(bookBorrow);
-
-            return ResponseEntity.ok().build();
-
-        }
+        return ResponseEntity.ok().build();
     }
 }
